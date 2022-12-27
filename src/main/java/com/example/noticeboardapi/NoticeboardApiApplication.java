858666c743044ffc76a31eef127edffa377435a3
seokho <seokho7799@gmail.com>
2022-12-27 16:19:03 +0900

    프로젝트 초기설정

---
+++ src/main/java/com/example/noticeboardapi/NoticeboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeboardApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noticeboardapi/NoticeboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
