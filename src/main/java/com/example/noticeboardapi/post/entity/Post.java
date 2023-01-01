185a2c6ee6e73295a7f65b01158f3519c5475713
server <seokho7799@gmail.com>
2023-01-02 03:08:43 +0900

    fix constructor parameter and static factory method parameter

--- src/main/java/com/example/noticeboardapi/post/entity/Post.java
+++ src/main/java/com/example/noticeboardapi/post/entity/Post.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static javax.persistence.CascadeType.ALL;
@@ -33,23 +34,22 @@ public class Post {
 
     @OneToMany(fetch = LAZY, cascade = ALL, orphanRemoval = true)
     @JoinColumn(name = "post_id")
-    private List<PostFile> postFiles;
+    private List<PostFile> postFiles = new ArrayList<>();
     private LocalDateTime lastModifiedTime;
     private LocalDateTime createdTime;
     private Integer reportCount;
     private Integer viewCount;
     private Integer recommendationCount;
 
-    public Post(String author, Category category, String title, String text, String imageLink, String videoLink) {
+    public Post(String author, Category category, String title, String text, List<PostFile> postFiles) {
         this.author = author;
         this.category = category;
         this.text = text;
-        this.imageLink = imageLink;
-        this.videoLink = videoLink;
+        this.postFiles.addAll(postFiles);
     }
 
-    public static Post createPostByFormat(String author, Category category, String title
-                                          String text, String imageLink, String videoLink) {
-        return new Post(author, category, title, text, imageLink, videoLink);
+    public static Post createPostByFormat(String author, Category category, String title,
+                                          String text, List<PostFile> postFiles) {
+        return new Post(author, category, title, text, postFiles);
     }
 }
