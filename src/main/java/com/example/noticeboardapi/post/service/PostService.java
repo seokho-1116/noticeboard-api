eccf3964c314ecc217d6be47a747d57e53d61f34
seokho <seokho7799@gmail.com>
2023-01-02 11:22:54 +0900

    add getPost function add PostThumbnailDto

--- src/main/java/com/example/noticeboardapi/post/service/PostService.java
+++ src/main/java/com/example/noticeboardapi/post/service/PostService.java
@@ -6,7 +6,10 @@ import com.example.noticeboardapi.post.entity.Post;
 import com.example.noticeboardapi.post.entity.PostFile;
 import com.example.noticeboardapi.post.repository.PostJpaRepository;
 import com.example.noticeboardapi.post.repository.PostQueryRepository;
+import com.example.noticeboardapi.post.service.dto.PostThumbnailDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,14 +21,20 @@ import java.util.List;
 public class PostService {
 
     private final PostJpaRepository postJpaRepository;
+    private final PostQueryRepository postQueryRepository;
     private final FileStore fileStore;
 
-    @Transactional
     public Long savePost(PostFormat postFormat) {
         List<PostFile> attachFiles = fileStore.storeFiles(postFormat.getAttachFiles());
         Post post = Post.createPostByFormat(postFormat.getAuthor(), postFormat.getCategory(),
                 postFormat.getText(), postFormat.getTitle(), attachFiles);
         Post savedPost = postJpaRepository.save(post);
         return savedPost.getId();
+    }
+
+    public Page<PostThumbnailDto> getPosts(Pageable pageable) {
+        Page<Post> posts = postQueryRepository.find10PostsByPaging(pageable);
+        return posts.map(p -> PostThumbnailDto.createPostThumbnailDto(p.getId(), p.getAuthor(), p.getCategory(),
+                p.getTitle(), p.getText(), p.getPostFiles()));
     }
 }
---
+++ src/main/java/com/example/noticeboardapi/post/service/dto/PostThumbnailDto.java
@@ -0,0 +1,24 @@
+package com.example.noticeboardapi.post.service.dto;
+
+import com.example.noticeboardapi.post.entity.Category;
+import com.example.noticeboardapi.post.entity.PostFile;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class PostThumbnailDto {
+    private Long postId;
+    private String author;
+    private Category category;
+    private String title;
+    private String text;
+    private List<PostFile> postFiles;
+
+    public static PostThumbnailDto createPostThumbnailDto(Long postId, String author, Category category,
+                                                          String title, String text, List<PostFile> postFiles) {
+        return new PostThumbnailDto(postId, author, category, title, text, postFiles);
+    }
+}
