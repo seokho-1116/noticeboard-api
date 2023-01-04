4f3f1a3808ca9c473a5f1c1843a9c6bbb6099c88
seokho <seokho7799@gmail.com>
2023-01-04 16:37:15 +0900

    add jpa repo test

---
+++ src/test/java/com/example/noticeboardapi/post/repository/PostJpaRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.example.noticeboardapi.post.repository;
+
+import com.example.noticeboardapi.post.entity.Category;
+import com.example.noticeboardapi.post.entity.Post;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PostJpaRepositoryTest {
+    @Autowired
+    private PostJpaRepository postJpaRepository;
+
+    @Test
+    void saveTest() {
+        Post savedPost = postJpaRepository.save(Post.createPostByFormat("seokho", Category.LOL, "TITLE", "TEXT"));
+        assertThat(savedPost.getId()).isEqualTo(1001);
+    }
+}
