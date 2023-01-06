958ccbed83c500e03920011707ef94fdcb4ae34b
seokho <seokho7799@gmail.com>
2023-01-06 15:08:27 +0900

    fix findPostById method and test

--- src/main/java/com/example/noticeboardapi/post/repository/PostQueryRepository.java
+++ src/main/java/com/example/noticeboardapi/post/repository/PostQueryRepository.java
@@ -1,6 +1,7 @@
 package com.example.noticeboardapi.post.repository;
 
 import com.example.noticeboardapi.post.entity.Post;
+import com.example.noticeboardapi.post.entity.PostFile;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.springframework.data.domain.Page;
@@ -42,10 +43,21 @@ public class PostQueryRepository {
         return new PageImpl<>(posts, pageable, total);
     }
 
-    public Post findPostById(Long pageNo) {
-        return dslContext.select(POST.asterisk())
+    public Post findPostById(Long postNo) {
+        return dslContext.select(POST.POST_ID, POST.AUTHOR, POST.TITLE,
+                        POST.CREATED_TIME, POST.CATEGORY, POST.RECOMMENDATION_COUNT,
+                        POST.VIEW_COUNT, POST.TEXT,
+                        multisetAgg(
+                                POST_FILE.FILE_ID,
+                                POST_FILE.FILE_TYPE,
+                                POST_FILE.STORE_FILE_NAME,
+                                POST_FILE.UPLOAD_FILE_NAME
+                        ).as("post_files").convertFrom(r -> r.into(PostFile.class)))
                 .from(POST)
-                .where(POST.POST_ID.eq(pageNo))
+                .join(POST_FILE)
+                .on(POST.POST_ID.eq(POST_FILE.POST_ID))
+                .groupBy(POST.POST_ID)
+                .having(POST.POST_ID.eq(postNo))
                 .fetchOneInto(Post.class);
     }
 }
--- src/test/java/com/example/noticeboardapi/post/repository/PostQueryRepositoryTest.java
+++ src/test/java/com/example/noticeboardapi/post/repository/PostQueryRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.noticeboardapi.post.repository;
 import com.example.noticeboardapi.post.entity.Post;
 import com.example.noticeboardapi.post.entity.PostFile;
 import org.jooq.DSLContext;
+import org.jooq.generated.test.tables.Comment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.jooq.generated.test.tables.Comment.COMMENT;
 import static org.jooq.generated.test.tables.Post.POST;
 import static org.jooq.generated.test.tables.PostFile.POST_FILE;
 import static org.jooq.impl.DSL.*;
@@ -58,14 +60,24 @@ public class PostQueryRepositoryTest {
     @Test
     @DisplayName("게시글 번호로 특정 게시글 요청 테스트")
     void requestSpecificPost() {
-        Long pageNo = 1L;
-        Post post = dslContext.select(POST.asterisk())
+        Long postNo = 1L;
+        Post post = dslContext.select(POST.POST_ID, POST.AUTHOR, POST.TITLE,
+                        POST.CREATED_TIME, POST.CATEGORY, POST.RECOMMENDATION_COUNT,
+                        POST.VIEW_COUNT, POST.TEXT,
+                        multisetAgg(
+                                POST_FILE.FILE_ID,
+                                POST_FILE.FILE_TYPE,
+                                POST_FILE.STORE_FILE_NAME,
+                                POST_FILE.UPLOAD_FILE_NAME
+                        ).as("post_files").convertFrom(r -> r.into(PostFile.class)))
                 .from(POST)
-                .where(POST.POST_ID.eq(pageNo))
+                .join(POST_FILE)
+                .on(POST.POST_ID.eq(POST_FILE.POST_ID))
+                .groupBy(POST.POST_ID)
+                .having(POST.POST_ID.eq(postNo))
                 .fetchOneInto(Post.class);
 
         assertThat(post).isNotNull();
         assertThat(post.getId()).isEqualTo(1L);
     }
-
 }
