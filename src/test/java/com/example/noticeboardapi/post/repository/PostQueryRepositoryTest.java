1589ef5935554e90f3a2f261b3b638e926df9d8a
seokho <seokho7799@gmail.com>
2023-01-06 13:40:55 +0900

    fix jooq paging query

--- src/test/java/com/example/noticeboardapi/post/repository/PostQueryRepositoryTest.java
+++ src/test/java/com/example/noticeboardapi/post/repository/PostQueryRepositoryTest.java
@@ -2,9 +2,7 @@ package com.example.noticeboardapi.post.repository;
 
 import com.example.noticeboardapi.post.entity.Post;
 import com.example.noticeboardapi.post.entity.PostFile;
-import com.example.noticeboardapi.post.service.dto.PostThumbnailDto;
 import org.jooq.DSLContext;
-import org.jooq.Records;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -33,9 +30,20 @@ public class PostQueryRepositoryTest {
     @DisplayName("메인 페이지에서 게시글 10개의 썸네일 목록을 최신순으로 보여주기 테스트")
     void request10PostsThumbnailOrderByCreatedTime() {
         Pageable pageable = PageRequest.of(0, 10);
-        List<Post> posts = dslContext.select(POST.POST_ID, POST.AUTHOR, POST.TITLE, POST.CREATED_TIME, POST.CATEGORY, POST.RECOMMENDATION_COUNT)
+
+        List<Post> posts = dslContext
+                .select(POST.POST_ID, POST.AUTHOR, POST.TITLE, POST.CREATED_TIME, POST.CATEGORY, POST.RECOMMENDATION_COUNT,
+                        multisetAgg(
+                                POST_FILE.FILE_ID,
+                                POST_FILE.FILE_TYPE,
+                                POST_FILE.STORE_FILE_NAME,
+                                POST_FILE.UPLOAD_FILE_NAME
+                        ).as("post_files").convertFrom(r -> r.into(PostFile.class)))
                 .from(POST)
-                .where(POST.POST_ID.gt(pageable.getOffset()))
+                .join(POST_FILE)
+                .on(POST.POST_ID.eq(POST_FILE.POST_ID))
+                .groupBy(POST.POST_ID)
+                .having(POST.POST_ID.gt(pageable.getOffset()))
                 .limit(pageable.getPageSize())
                 .fetchInto(Post.class);
 
@@ -44,6 +52,7 @@ public class PostQueryRepositoryTest {
         Page<Post> result = new PageImpl<>(posts, pageable, total);
 
         assertThat(result.getNumberOfElements()).isEqualTo(10);
+        assertThat(result).allMatch(r -> r.getCreatedTime() != null);
     }
 
     @Test
@@ -59,49 +68,4 @@ public class PostQueryRepositoryTest {
         assertThat(post.getId()).isEqualTo(1L);
     }
 
-
-    @Test
-    void request10Posts() {
-
-        Pageable pageable = PageRequest.of(0, 10);
-
-        List<PostThumbnailDto> postThumbnailDtos = dslContext
-                .select(POST.POST_ID, POST.AUTHOR, POST.TITLE, POST.CREATED_TIME, POST.CATEGORY, POST.RECOMMENDATION_COUNT,
-                multiset(
-                        select(POST_FILE.FILE_ID)
-                                .from(POST_FILE)
-                                .where(POST_FILE.POST_ID.eq(POST.POST_ID))
-                ).as("postFiles"))
-                .from(POST)
-                .where(POST.POST_ID.gt(pageable.getOffset()))
-                .limit(pageable.getPageSize())
-                .fetchInto(PostThumbnailDto.class);
-
-        int total = dslContext.fetchCount(POST);
-
-        Page<PostThumbnailDto> result = new PageImpl<>(postThumbnailDtos, pageable, total);
-
-        assertThat(result.getNumberOfElements()).isEqualTo(10);
-    }
-
-    @Test
-    void request10Posts2() {
-
-        Pageable pageable = PageRequest.of(0, 10);
-
-        List<PostThumbnailDto> postThumbnailDtos = dslContext
-                .select(POST.POST_ID, POST.AUTHOR, POST.TITLE, POST.CREATED_TIME, POST.CATEGORY, POST.RECOMMENDATION_COUNT,
-                field(select(jsonArrayAgg(jsonObject(POST_FILE.FILE_ID, POST_FILE.FILE_TYPE, POST_FILE.STORE_FILE_NAME, POST_FILE.UPLOAD_FILE_NAME)))
-                        .from(POST_FILE).join(POST).on(POST.POST_ID.eq(POST_FILE.POST_ID))).as("postFiles"))
-                .from(POST)
-                .where(POST.POST_ID.gt(pageable.getOffset()))
-                .limit(pageable.getPageSize())
-                .fetchInto(PostThumbnailDto.class);
-
-        int total = dslContext.fetchCount(POST);
-
-        Page<PostThumbnailDto> result = new PageImpl<>(postThumbnailDtos, pageable, total);
-
-        assertThat(result.getNumberOfElements()).isEqualTo(10);
-    }
 }
