df9bf89fba7573f0263b21ec088a55c51b889d8e
seokho <seokho7799@gmail.com>
2023-01-06 13:39:43 +0900

    add savePostFileTest

--- src/test/java/com/example/noticeboardapi/post/repository/PostJpaRepositoryTest.java
+++ src/test/java/com/example/noticeboardapi/post/repository/PostJpaRepositoryTest.java
@@ -2,12 +2,17 @@ package com.example.noticeboardapi.post.repository;
 
 import com.example.noticeboardapi.post.entity.Category;
 import com.example.noticeboardapi.post.entity.Post;
+import com.example.noticeboardapi.post.entity.PostFile;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,10 +22,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PostJpaRepositoryTest {
     @Autowired
     private PostJpaRepository postJpaRepository;
+    @Autowired
+    private PostFileJpaRepository postFileJpaRepository;
 
     @Test
     void saveTest() {
-        Post savedPost = postJpaRepository.save(Post.createPostByFormat("seokho", Category.LOL, "TITLE", "TEXT"));
+        Post savedPost = postJpaRepository.save(Post.createPostByFormat("seokho", Category.LOL, "TITLE", "TEXT", Collections.emptyList()));
         assertThat(savedPost.getId()).isEqualTo(1001);
+    }
+
+    @Test
+    void savePostFileTest() {
+        postJpaRepository.save(Post.createPostByFormat("seokho", Category.LOL, "TITLE",
+                "TEXT", List.of(new PostFile("jpg", "hi", "Hello"))));
+        Optional<PostFile> postFile = postFileJpaRepository.findById(501L);
+        assertThat(postFile.isPresent()).isTrue();
     }
 }
