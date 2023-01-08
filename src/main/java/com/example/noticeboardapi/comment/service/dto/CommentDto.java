7b6b4e6989a0c9bc111bc1a5cea0d13b24ab2b71
server <seokho7799@gmail.com>
2023-01-08 10:23:35 +0900

    add comment dto

---
+++ src/main/java/com/example/noticeboardapi/comment/service/dto/CommentDto.java
@@ -0,0 +1,14 @@
+package com.example.noticeboardapi.comment.service.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CommentDto {
+    private String author;
+    private String text;
+    private LocalDateTime createdTime;
+}
