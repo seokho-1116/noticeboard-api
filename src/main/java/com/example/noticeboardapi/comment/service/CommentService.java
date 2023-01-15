28ee1021bc3aa5bec171ebb520bf00ea69f1e54f
server <seokho7799@gmail.com>
2023-01-15 22:26:14 +0900

    add saveReply add deleteComment

--- src/main/java/com/example/noticeboardapi/comment/service/CommentService.java
+++ src/main/java/com/example/noticeboardapi/comment/service/CommentService.java
@@ -2,7 +2,9 @@ package com.example.noticeboardapi.comment.service;
 
 import com.example.noticeboardapi.comment.controller.CommentFormat;
 import com.example.noticeboardapi.comment.entity.Comment;
+import com.example.noticeboardapi.comment.repository.CommentCommandRepository;
 import com.example.noticeboardapi.comment.repository.CommentJpaRepository;
+import com.example.noticeboardapi.comment.repository.CommentQueryRepository;
 import com.example.noticeboardapi.comment.service.dto.CommentDto;
 import com.example.noticeboardapi.comment.service.dto.CommentMapper;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import java.time.LocalDateTime;
 public class CommentService {
 
     private final CommentJpaRepository commentJpaRepository;
+    private final CommentCommandRepository commentCommandRepository;
     private final CommentMapper commentMapper = CommentMapper.INSTANCE;
 
     public CommentDto saveComment(Long postNo, CommentFormat commentFormat) {
@@ -22,5 +25,18 @@ public class CommentService {
                 Comment.createCommentByFormat(postNo, commentFormat.getAuthor(), commentFormat.getText(), LocalDateTime.now())
         );
         return commentMapper.fromComment(savedComment);
+    }
+
+    public CommentDto saveReply(Long postNo, Long parentCommentNo, CommentFormat commentFormat) {
+        Comment savedComment = commentCommandRepository.saveReply(
+                Comment.createCommentByFormat(postNo, commentFormat.getAuthor(), commentFormat.getText(), LocalDateTime.now()),
+                parentCommentNo
+        );
+        return commentMapper.fromComment(savedComment);
+    }
+
+    public CommentDto deleteComment(Long postNo, Long commentNo) {
+        Comment deletedComment = commentCommandRepository.deleteComment(postNo, commentNo);
+        return commentMapper.fromComment(deletedComment);
     }
 }
