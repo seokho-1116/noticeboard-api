71c971a4cf84f63cd31e29311a27ae889b01570f
server <seokho7799@gmail.com>
2023-01-15 22:18:50 +0900

    update commentDeleteTest update replySaveTest

--- src/test/java/com/example/noticeboardapi/comment/repository/CommentCommandRepositoryTest.java
+++ src/test/java/com/example/noticeboardapi/comment/repository/CommentCommandRepositoryTest.java
@@ -3,8 +3,8 @@ package com.example.noticeboardapi.comment.repository;
 import com.example.noticeboardapi.comment.entity.Comment;
 import com.example.noticeboardapi.comment.entity.TreePath;
 import org.jooq.DSLContext;
+import org.jooq.generated.test.tables.records.CommentRecord;
 import org.jooq.impl.DSL;
-import org.jooq.types.ULong;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,13 +28,14 @@ public class CommentCommandRepositoryTest {
     @ParameterizedTest
     @ValueSource(longs = {1L})
     void commentDeleteTest(Long commentNo) {
+        Long postNo = 1L;
         dslContext.update(COMMENT)
                 .set(COMMENT.TEXT, "삭제된 댓글입니다.")
-                .where(COMMENT.COMMENT_ID.eq(ULong.valueOf(commentNo)))
+                .where(COMMENT.POST_ID.eq(postNo).and(COMMENT.COMMENT_ID.eq(commentNo)))
                 .execute();
 
         Comment comment = dslContext.selectFrom(COMMENT)
-                .where(COMMENT.COMMENT_ID.eq(ULong.valueOf(commentNo)))
+                .where(COMMENT.COMMENT_ID.eq(commentNo))
                 .fetchOneInto(Comment.class);
 
         assertThat(comment.getText()).isEqualTo("삭제된 댓글입니다.");
@@ -42,27 +43,31 @@ public class CommentCommandRepositoryTest {
 
     @ParameterizedTest
     @ValueSource(longs = {1L})
-    void replyCommentCreateTest(Long postNo) {
+    void replySaveTest(Long postNo) {
         long parentCommentNo = 20L;
 
-        Long commentNo = dslContext
-                .insertInto(COMMENT, COMMENT.POST_ID, COMMENT.AUTHOR, COMMENT.TEXT, COMMENT.CREATED_TIME)
-                .values(1L, "seokho", "hi", LocalDateTime.now())
+        CommentRecord commentRecord = dslContext
+                .insertInto(COMMENT, COMMENT.COMMENT_ID, COMMENT.POST_ID, COMMENT.AUTHOR, COMMENT.TEXT, COMMENT.CREATED_TIME)
+                .values(1003L, 1L, "seokho", "hi", LocalDateTime.now())
                 .returning(COMMENT.COMMENT_ID)
-                .fetchOneInto(Long.class);
+                .fetchOne();
+        Long commentNo = (Long) commentRecord.getValue("comment_id");
 
         dslContext.insertInto(TREE_PATH)
                 .select(DSL.select(DSL.val(postNo), TREE_PATH.ANCESTOR, DSL.val(commentNo), TREE_PATH.DEPTH.plus(1))
                         .from(TREE_PATH)
-                        .where(TREE_PATH.DESCENDANT.eq(ULong.valueOf(parentCommentNo)))
-                        .unionAll(DSL.select(DSL.val(postNo), DSL.val(ULong.valueOf(commentNo)), DSL.val(commentNo), DSL.val(0L))))
+                        .where(TREE_PATH.DESCENDANT.eq(parentCommentNo))
+                        .unionAll(DSL.select(DSL.val(postNo), DSL.val(commentNo), DSL.val(commentNo), DSL.val(0))))
                 .execute();
 
-        List<TreePath> treePaths = dslContext.selectFrom(TREE_PATH)
-                .where(TREE_PATH.DESCENDANT.eq(ULong.valueOf(commentNo)))
+        List<TreePath> treePaths = dslContext
+                .select(TREE_PATH.asterisk())
+                .from(TREE_PATH)
+                .where(TREE_PATH.DESCENDANT.eq(commentNo))
+                .orderBy(TREE_PATH.DEPTH.desc())
                 .fetchInto(TreePath.class);
 
-        assertThat(commentNo).isEqualTo(1003);
-        assertThat(treePaths.stream().map(TreePath::getAncestor)).containsExactly(0L, 992L, 20L, 1003L);
+        assertThat(commentNo.intValue()).isEqualTo(1003);
+        assertThat(treePaths.stream().map(TreePath::getAncestor)).containsExactly(0L, 621L, 445L, 20L, 1003L);
     }
 }
