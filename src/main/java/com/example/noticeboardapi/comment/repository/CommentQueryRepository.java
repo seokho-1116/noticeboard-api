5cc133bd13779311798d3fdcacc6ccdbbaf214cb
seokho <seokho7799@gmail.com>
2023-01-17 13:08:42 +0900

    rename variable cc1, cc2 to tp1, tp2

--- src/main/java/com/example/noticeboardapi/comment/repository/CommentQueryRepository.java
+++ src/main/java/com/example/noticeboardapi/comment/repository/CommentQueryRepository.java
@@ -23,26 +23,26 @@ public class CommentQueryRepository {
 
     private final DSLContext dslContext;
 
-    public Page<Comment> find20CommentsByPaging(Long postNo, Pageable pageable) {
+    public Page<Comment> find20Comments(Long postNo, Pageable pageable) {
         org.jooq.generated.test.tables.Comment c1 = new org.jooq.generated.test.tables.Comment("c1");
         org.jooq.generated.test.tables.Comment c2 = new org.jooq.generated.test.tables.Comment("c2");
-        TreePath cc1 = new TreePath("cc1");
-        TreePath cc2 = new TreePath("cc2");
+        TreePath tp1 = new TreePath("tp1");
+        TreePath tp2 = new TreePath("tp2");
         TreePath breadcrumb = new TreePath("breadcrumb");
 
         List<Comment> comments = dslContext
                 .select(
                         c2.asterisk(),
-                        cc2.ANCESTOR,
+                        tp2.ANCESTOR,
                         groupConcat(breadcrumb.ANCESTOR).orderBy(breadcrumb.DEPTH.desc()).as("breadcrumbs")
                 )
                 .from(c1)
-                .join(cc1).on(cc1.ANCESTOR.eq(c1.COMMENT_ID).and(cc1.POST_ID.eq(postNo)).and(c1.POST_ID.eq(postNo)))
-                .join(c2).on(cc1.DESCENDANT.eq(c2.COMMENT_ID).and(c2.POST_ID.eq(postNo)))
-                .leftOuterJoin(cc2).on(cc2.DESCENDANT.eq(c2.COMMENT_ID).and(cc2.DEPTH.eq(1)))
-                .join(breadcrumb).on(cc1.DESCENDANT.eq(breadcrumb.DESCENDANT))
+                .join(tp1).on(tp1.ANCESTOR.eq(c1.COMMENT_ID).and(tp1.POST_ID.eq(postNo)).and(c1.POST_ID.eq(postNo)))
+                .join(c2).on(tp1.DESCENDANT.eq(c2.COMMENT_ID).and(c2.POST_ID.eq(postNo)))
+                .leftOuterJoin(tp2).on(tp2.DESCENDANT.eq(c2.COMMENT_ID).and(tp2.DEPTH.eq(1)))
+                .join(breadcrumb).on(tp1.DESCENDANT.eq(breadcrumb.DESCENDANT))
                 .where(c1.COMMENT_ID.eq(0L))
-                .groupBy(cc1.DESCENDANT)
+                .groupBy(tp1.DESCENDANT)
                 .orderBy(DSL.field("breadcrumbs"))
                 .limit(pageable.getPageSize())
                 .offset(pageable.getOffset())
@@ -53,26 +53,26 @@ public class CommentQueryRepository {
         return new PageImpl<>(comments, pageable, count);
     }
 
-    public Page<Comment> findPageContainingSpecificComment(Long postNo, Long commentNo) {
-        TreePath cc1 = new TreePath("cc1");
-        TreePath cc2 = new TreePath("cc2");
+    public Page<Comment> find20CommentsContainingSpecificComment(Long postNo, Long commentNo) {
+        TreePath tp1 = new TreePath("tp1");
+        TreePath tp2 = new TreePath("tp2");
         TreePath breadcrumb = new TreePath("breadcrumb");
 
         Long rowNum = dslContext
                 .select(field("r.row_num"))
                 .from(select(rowNumber().over(orderBy(field("breadcrumbs"))).as("row_num"),
-                        cc1.DESCENDANT,
+                        tp1.DESCENDANT,
                         groupConcat(breadcrumb.ANCESTOR).orderBy(breadcrumb.DEPTH.desc()).as("breadcrumbs"))
-                        .from(cc1)
-                        .leftOuterJoin(cc2).on(cc2.DESCENDANT.eq(cc1.DESCENDANT).and(cc2.DEPTH.eq(1)))
-                        .join(breadcrumb).on(cc1.DESCENDANT.eq(breadcrumb.DESCENDANT))
-                        .where(cc1.ANCESTOR.eq(0L))
-                        .groupBy(cc1.DESCENDANT)
+                        .from(tp1)
+                        .leftOuterJoin(tp2).on(tp2.DESCENDANT.eq(tp1.DESCENDANT).and(tp2.DEPTH.eq(1)))
+                        .join(breadcrumb).on(tp1.DESCENDANT.eq(breadcrumb.DESCENDANT))
+                        .where(tp1.ANCESTOR.eq(0L))
+                        .groupBy(tp1.DESCENDANT)
                         .orderBy(field("breadcrumbs")).asTable("r"))
                 .where("r.descendant = " + commentNo.toString())
                 .fetchOneInto(Long.class);
 
         int page = rowNum.intValue() / 20;
-        return find20CommentsByPaging(postNo, PageRequest.of(page, 20));
+        return find20Comments(postNo, PageRequest.of(page, 20));
     }
 }
