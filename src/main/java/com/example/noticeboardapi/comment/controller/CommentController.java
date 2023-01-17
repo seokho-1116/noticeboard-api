57b898c37c1875fc710184bcf5dc7ac8a9dfb9f6
seokho <seokho7799@gmail.com>
2023-01-17 13:08:09 +0900

    add recommendationCountAdd method

--- src/main/java/com/example/noticeboardapi/comment/controller/CommentController.java
+++ src/main/java/com/example/noticeboardapi/comment/controller/CommentController.java
@@ -1,11 +1,12 @@
 package com.example.noticeboardapi.comment.controller;
 
-import com.example.noticeboardapi.comment.service.CommentReadModel;
-import com.example.noticeboardapi.comment.service.CommentService;
+import com.example.noticeboardapi.comment.service.CommentReadService;
+import com.example.noticeboardapi.comment.service.CommentCommandService;
 import com.example.noticeboardapi.comment.service.dto.CommentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,32 +16,38 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class CommentController {
 
-    private final CommentService commentService;
-    private final CommentReadModel commentReadModel;
+    private final CommentCommandService commentCommandService;
+    private final CommentReadService commentReadService;
 
     @PostMapping("/posts/{postNo}/comments")
     public ResponseEntity<?> commentSave(@PathVariable Long postNo, @RequestBody CommentFormat commentFormat) {
-        CommentDto commentDto = commentService.saveComment(postNo, commentFormat);
+        CommentDto commentDto = commentCommandService.saveComment(postNo, commentFormat);
         return ResponseEntity.created(URI.create("/posts/" + postNo + "/comments"))
-                .body(commentReadModel.getPageOf20CommentsContainingSpecificComment(postNo, commentDto));
+                .body(commentReadService.find20CommentsContainingSpecificComment(postNo, commentDto));
     }
 
     @PostMapping("/posts/{postNo}/comments/{parentCommentId}/comments")
     public ResponseEntity<?> replySave(@PathVariable Long postNo, @PathVariable Long parentCommentId,
                                        @RequestBody CommentFormat commentFormat) {
-        CommentDto commentDto = commentService.saveReply(postNo, parentCommentId, commentFormat);
+        CommentDto commentDto = commentCommandService.saveReply(postNo, parentCommentId, commentFormat);
         return ResponseEntity.created(URI.create("/posts/" + postNo + "/comments/" + parentCommentId + "/comments"))
-                .body(commentReadModel.getPageOf20CommentsContainingSpecificComment(postNo, commentDto));
+                .body(commentReadService.find20CommentsContainingSpecificComment(postNo, commentDto));
     }
 
     @GetMapping("/posts/{postNo}/comments")
     public ResponseEntity<?> commentList(@PathVariable Long postNo, @PageableDefault Pageable pageable) {
-        return ResponseEntity.ok(commentReadModel.getPageOf20Comments(postNo, pageable));
+        return ResponseEntity.ok(commentReadService.find20Comments(postNo, pageable));
     }
 
     @DeleteMapping("/posts/{postNo}/comments/{commentNo}")
     public ResponseEntity<?> commentDelete(@PathVariable Long postNo, @PathVariable Long commentNo) {
-        commentService.deleteComment(postNo, commentNo);
+        commentCommandService.deleteComment(postNo, commentNo);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/posts/{postNo}/comments/{commentNo}/recommendation")
+    public ResponseEntity<?> recommendationCountAdd(@PathVariable Long postNo, @PathVariable Long commentNo) {
+        commentCommandService.addRecommendationCount(postNo, commentNo);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
