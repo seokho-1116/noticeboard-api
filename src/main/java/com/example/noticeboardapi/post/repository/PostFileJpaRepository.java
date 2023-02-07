bf53a05cfda46e414b809aa06e5eeec1603c26a1
seokho <seokho7799@gmail.com>
2023-02-07 16:23:44 +0900

    ?

--- src/main/java/com/example/noticeboardapi/post/repository/PostFileJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.noticeboardapi.post.repository;
-
-import com.example.noticeboardapi.post.entity.PostFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostFileJpaRepository extends JpaRepository<PostFile, Long> {
-}
