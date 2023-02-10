5c9ee5d9465d3bac9e36a0f53da8e53a97e7a5b0
seokho <seokho7799@gmail.com>
2023-02-10 09:50:19 +0900

    delete original savePostTest change savePostFileTest to savePostTest and fix

--- src/test/java/com/example/noticeboardapi/post/repository/PostJpaRepositoryTest.java
+++ src/test/java/com/example/noticeboardapi/post/repository/PostJpaRepositoryTest.java
@@ -9,9 +9,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,20 +19,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PostJpaRepositoryTest {
     @Autowired
     private PostJpaRepository postJpaRepository;
-    @Autowired
-    private PostFileJpaRepository postFileJpaRepository;
 
     @Test
-    void saveTest() {
-        Post savedPost = postJpaRepository.save(Post.createPostByFormat("seokho", Category.LOL, "TITLE", "TEXT", Collections.emptyList()));
-        assertThat(savedPost.getId()).isEqualTo(1001);
-    }
-
-    @Test
-    void savePostFileTest() {
-        postJpaRepository.save(Post.createPostByFormat("seokho", Category.LOL, "TITLE",
+    void savePostTest() {
+        Post savedPost = postJpaRepository.save(Post.createPostByFormat("seokho", Category.LOL, "TITLE",
                 "TEXT", List.of(new PostFile("jpg", "hi", "Hello"))));
-        Optional<PostFile> postFile = postFileJpaRepository.findById(501L);
-        assertThat(postFile.isPresent()).isTrue();
+        assertThat(savedPost).isNotNull();
+        assertThat(savedPost.getPostFiles().size()).isEqualTo(1);
     }
 }
