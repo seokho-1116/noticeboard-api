dad84036ecf640e192c135496583e623187aa31f
server <seokho7799@gmail.com>
2023-02-19 18:28:27 +0900

    refactoring extract getCommentRecord method

--- src/main/java/com/example/noticeboardapi/domain/comment/repository/CommentCommandRepository.java
+++ src/main/java/com/example/noticeboardapi/domain/comment/repository/CommentCommandRepository.java
@@ -16,44 +16,29 @@ import static org.jooq.generated.test.tables.TreePath.TREE_PATH;
 @RequiredArgsConstructor
 public class CommentCommandRepository {
 
-    private CommentQueryRepository commentQueryRepository;
-    private CommentJpaRepository commentJpaRepository;
-    private DSLContext dslContext;
-
-    public Comment saveReply(Comment comment, Long parentCommentNo) {
-            Comment saveComment = commentJpaRepository.save(comment);
-
-            dslContext.insertInto(TREE_PATH)
-                    .select(DSL.select(DSL.val(comment.getPostId()), TREE_PATH.ANCESTOR,
-                                    DSL.val(saveComment.getId()), TREE_PATH.DEPTH.plus(1))
-                            .from(TREE_PATH)
-                            .where(TREE_PATH.DESCENDANT.eq(ULong.valueOf(parentCommentNo)))
-                        .unionAll(DSL.select(DSL.val(comment.getPostId()), DSL.val(ULong.valueOf(saveComment.getId())),
-                                DSL.val(saveComment.getId()), DSL.val(0)))
-                ).execute();
-
-        return saveComment;
-    }
+    private final CommentQueryRepository commentQueryRepository;
+    private final CommentJpaRepository commentJpaRepository;
+    private final DSLContext dslContext;
 
     public void deleteComment(Long postNo, Long commentNo) {
-        CommentRecord commentRecord = dslContext.fetchOne(COMMENT, COMMENT.POST_ID.eq(postNo)
-                .and(COMMENT.COMMENT_ID.eq(ULong.valueOf(commentNo))));
-
-        if (commentRecord == null) {
-            throw new NoSuchCommentException();
-        }
+        CommentRecord commentRecord = getCommentRecord(postNo, commentNo);
 
         commentRecord.setText("삭제된 댓글 입니다.");
         commentRecord.store();
     }
 
-    public void updateRecommendationCount(Long postNo, Long commentNo) {
+    private CommentRecord getCommentRecord(Long postNo, Long commentNo) {
         CommentRecord commentRecord = dslContext.fetchOne(COMMENT, COMMENT.POST_ID.eq(postNo)
                 .and(COMMENT.COMMENT_ID.eq(ULong.valueOf(commentNo))));
 
         if (commentRecord == null) {
             throw new NoSuchCommentException();
         }
+        return commentRecord;
+    }
+
+    public void updateRecommendationCount(Long postNo, Long commentNo) {
+        CommentRecord commentRecord = getCommentRecord(postNo, commentNo);
 
         commentRecord.setRecommendationCount(commentRecord.getRecommendationCount() + 1);
         commentRecord.store();
