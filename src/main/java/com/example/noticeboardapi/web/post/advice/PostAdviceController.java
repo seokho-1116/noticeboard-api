32dc9a84d6f73ad7096e25c311790606a9753a3a
server <seokho7799@gmail.com>
2023-02-19 15:42:20 +0900

    add value on @RestControllerAdvice

--- src/main/java/com/example/noticeboardapi/web/post/advice/PostAdviceController.java
+++ src/main/java/com/example/noticeboardapi/web/post/advice/PostAdviceController.java
@@ -1,7 +1,7 @@
 package com.example.noticeboardapi.web.post.advice;
 
-import com.example.noticeboardapi.domain.comment.exception.NoSuchCommentExcpetion;
 import com.example.noticeboardapi.domain.post.exception.NoSuchPostException;
+import com.example.noticeboardapi.web.post.PostController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,11 +12,11 @@ import org.springframework.web.multipart.support.MissingServletRequestPartExcept
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = PostController.class)
 public class PostAdviceController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NoSuchCommentExcpetion.class)
+    @ExceptionHandler(NoSuchPostException.class)
     public ResponseEntity<String> noSuchPostExceptionHandle(NoSuchPostException e, HttpServletRequest request) {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
