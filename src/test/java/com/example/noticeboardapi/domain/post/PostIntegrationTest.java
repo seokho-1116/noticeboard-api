155d7ad235a1083a38b269bee9fab067f5d7d9e0
server <seokho7799@gmail.com>
2023-02-19 15:44:52 +0900

    add ExceptionHandleTest on NoSuchPostException, MethodArgumentNotValidException, MissingServletRequestPartException

--- src/test/java/com/example/noticeboardapi/domain/post/PostIntegrationTest.java
+++ src/test/java/com/example/noticeboardapi/domain/post/PostIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PostIntegrationTest {
 
     @Autowired
-    MockMvc mvc;
+    private MockMvc mvc;
 
     @Autowired
     private ObjectMapper objectMapper;
--- src/test/java/com/example/noticeboardapi/web/post/PostControllerTest.java
+++ src/test/java/com/example/noticeboardapi/web/post/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.noticeboardapi.web.post;
 
 import com.example.noticeboardapi.domain.post.entity.Category;
+import com.example.noticeboardapi.domain.post.exception.NoSuchPostException;
 import com.example.noticeboardapi.domain.post.service.PostCommandService;
 import com.example.noticeboardapi.domain.post.service.PostReadService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -34,7 +36,7 @@ public class PostControllerTest {
     private PostReadService postReadService;
 
     @Test
-    @DisplayName("글쓰기 요청 테스트")
+    @DisplayName("게시글 생성 요청 테스트")
     void requestWritingPostTest() throws Exception {
         PostCreateFormat postCreateFormat = new PostCreateFormat();
         postCreateFormat.setAuthor("seokho");
@@ -52,11 +54,45 @@ public class PostControllerTest {
 
         mvc.perform(multipart("/posts")
                         .file(multipartFiles.get(0))
-                        .file(multipartFiles.get(1)))
-     //                   .file(postCreateFormatFile))
+                        .file(multipartFiles.get(1))
+                        .file(postCreateFormatFile))
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", "/posts/1"))
                 .andReturn();
+    }
+
+    @Test
+    @DisplayName("게시글 요청 폼 없이 게시글 생성 요청 예외 처리 테스트")
+    void requestWritingPostWithoutFormExceptionHandleTest() throws Exception {
+        mvc.perform(multipart("/posts"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("검증 실패인 게시글 요청 폼으로 게시글 생성 요청 예외 처리 테스트")
+    void requestWritingPostWithNotValidFormExceptionHandleTest() throws Exception {
+        PostCreateFormat postCreateFormat = new PostCreateFormat();
+        postCreateFormat.setAuthor("");
+        postCreateFormat.setCategory(Category.LOL);
+        postCreateFormat.setTitle("hi");
+        postCreateFormat.setText("hello");
+
+        MockMultipartFile postCreateFormatFile = new MockMultipartFile("postCreateFormat", "postCreateFormat",
+                "application/json", objectMapper.writeValueAsBytes(postCreateFormat));
+
+
+        mvc.perform(multipart("/posts")
+                        .file(postCreateFormatFile))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 특정 게시글 요청 예외 처리 테스트")
+    void requestSpecificPostExceptionHandleTest() throws Exception {
+        given(postReadService.findPost(anyLong())).willThrow(NoSuchPostException.class);
+
+        mvc.perform(get("/posts/1"))
+                .andExpect(status().isNotFound());
     }
 
 }
